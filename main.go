c0c0900358fce0961775afd6b15839b428903137
Feilner <github@oliver.4of.de>
2021-02-28 00:58:09 +0100

    disable tls certificate check
    In order to allow initial deployment disable certificate check.

--- main.go
+++ main.go
@@ -206,7 +206,9 @@ func main() {
 	}
 
 	ip := flag.Arg(0)
-	client := http.DefaultClient
+        customTransport := &(*http.DefaultTransport.(*http.Transport)) // make shallow copy
+        customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+        client := &http.Client{Transport: customTransport}
 	if len(*certFilePtr) == 0 {
 		*certFilePtr = filepath.Join(*basePathPtr, "ipmi_"+ip+".crt");
 	}
